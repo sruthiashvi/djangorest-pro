6c6ff8ba7f0431c0c2d2b3816f13413d06cc44fb
sruthiashvi <sruthianandofficial@gmail.com>
2019-08-02 16:29:46 +0530

    aswin

--- plans/admin.py
+++ plans/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Prepaidplans
-
+#aswin blocked me ... i am paavam.. pls get his number and give me
 # Register your models here.
 admin.site.register(Prepaidplans)
